e54ded266e85972fba1b8d4981551f310323cd72
unknown <1084400399@qq.com>
2018-07-11 10:43:26 +0800

    add dnsseeds

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -562,9 +562,9 @@ var TestNet2Params = Params{
 	Net:         wire.TestNet2,
 	DefaultPort: "12008",
 	DNSSeeds:    []string{
-		// "testnet1.h.cash",
-		// "testnet2.h.cash",
-		// "testnet3.h.cash",
+		 "testnet1.h.cash",
+		 "testnet2.h.cash",
+		 "testnet3.h.cash",
 	//"192.168.11.135",
 	},
 
